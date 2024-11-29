abb0157ca9176d4d83c91ef28462aba29221c612
Hamza <hamza_1904@outlook.com>
2024-11-29 15:10:11 +0400

    middleware

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const app = express();
+
+app.use(express.json());
+app.set('port', 3000);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, OPTIONS, POST, PUT');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+    
+}
