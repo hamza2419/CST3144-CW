05d3877fcdb0fe58297881807d766a9d4512bdaf
Hamza <hamza_1904@outlook.com>
2024-11-29 15:15:06 +0400

    mongodb connected

--- server.js
+++ server.js
@@ -13,3 +13,10 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
     next();
 });
+
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+MongoClient.connect('mongodb+srv://hamzaakhan24:mdx986868@cst3144.sq6yv.mongodb.net/', (err, client) => {
+    db = client.db('webstore');
+});
